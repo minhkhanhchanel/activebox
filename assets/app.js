8f6e3f0b0e220a7cec8a140adcd11d2d6a793a2a
nguyenminhhieummo <minhkhanhchanel@gmail.com>
2021-11-03 11:19:19 +0700

    ok

--- assets/app.js
+++ assets/app.js
@@ -13,14 +13,14 @@ const mobileIconModal = document.querySelector('.mobile-menu__ion-bar') //sai
 const mobileCloseModal = document.querySelector('.fa-times')//icon
 const mobileModalContainer = document.querySelector('.js-mobile-header-modal__container')
 function showMenuMobile() {
-    mobileModal.classList.add('open'),mobileIconModal.classList.add('fa-times')
+    mobileModal.classList.toggle('open'),mobileIconModal.classList.toggle('fa-times')
 }
-function hideMenuMobile() {
-    mobileModal.classList.remove('open'),mobileIconModal.classList.remove('fa-times')
-}
+// function hideMenuMobile() {
+//     mobileModal.classList.toggle('open'), mobileIconModal.classList.remove('fa-times')
+// }
 mobileMenu.addEventListener('click',showMenuMobile)
 
-mobileModal.addEventListener('click',hideMenuMobile)
+// mobileModal.addEventListener('click',hideMenuMobile)
 
 // mobileCloseModal.addEventListener('click',hideMenuMobile)
 
